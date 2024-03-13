b197ca895d19b289b825e645bcf4a739be7b0bef
Oleksandr <shurik99911@gmail.com>
2024-03-14 00:28:00 +0200

    git init

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import Markdoc from '@markdoc/markdoc';
+
+function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [htmlText, setHtmlText] = useState(null);
+
+  console.log(selectedFile);
+  console.log(htmlText);
+
+  useEffect(() => {
+    async function readText() {
+      if (selectedFile) {
+        const text = await selectedFile.text();
+
+        const ast = Markdoc.parse(text);
+        const content = Markdoc.transform(ast);
+        const html = Markdoc.renderers.html(content);
+
+        setHtmlText(html);
+      }
+    }
+
+    readText();
+  }, [selectedFile]);
+
+  return (
+    <div>
+      <p className="app">HELLO</p>
+      <input
+        type="file"
+        onChange={(e) => {
+          setSelectedFile(e.target.files[0]);
+        }}
+      />
+      <div dangerouslySetInnerHTML={{ __html: htmlText }}></div>
+    </div>
+  );
+}
+
+export default App;
